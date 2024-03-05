768f31f61ae9321b543ae6357920ec0b4d4203f5
root <root@ip-172-31-47-174.ec2.internal>
2024-03-05 14:55:45 +0000

    changed value 30 login.java

--- login.java
+++ login.java
@@ -3,8 +3,7 @@ public class login
  public static void main()
   {
     int i =0;
-    for (i=0;i<==20;i++)
-    { system.out printIn ("number: "+i);
+    for (i=0;i<==30("number: "+i);
     }    
      
   }
